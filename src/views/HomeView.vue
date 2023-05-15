1ebec994f3ba431ca4902b6438ac6f6536675531
GuztaJF-DS <noobshippuden13@gmail.com>
2023-05-15 14:44:34 -0300

    feat: updated the layout for mobile devices

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,11 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <div class="main">
-    <div class="left">
+    <div class="TextPart">
         <h2>Prazer, meu nome é Gustavo</h2>
         <h1>Um Dev Full-Stack </h1>
       </div>
-    <div class="right">
+    <div class="linksPart">
       <div class="wrapper">
         <nav>
           <RouterLink active-class="active" to="/about">Sobre Mim</RouterLink>
@@ -28,51 +28,63 @@ import { RouterLink } from 'vue-router'
     display: flex;
     justify-content: space-between;
     background-color: #0C4173;
+  }
+  .TextPart {
+    margin: 20px;
+    h2 {
+      margin-left: 7px;
+      width:30vw;
+      font-size: 3.07vw;
+    }
     h1 {
       margin-top: 150px;
       font-weight: 600;
       margin-left: 30px;
-      font-size: 66px;
+      font-size: 5vw;
     }
-    h2 {
-      margin-left: 7px;
-      width:400px;
-      font-size: 42px;
+    @media (max-width: 768px) {
+      h2 {
+        font-size: 23.57px;
+      }
+      h1 {
+        font-size: 38px;
+      }
+    }
+    @media (min-width: 1756px) {
+      h2 {
+        width:526.5px;
+        font-size: 53.9px;
+      }
+      h1 {
+        font-size: 87.8px;
+      }
     }
   }
-  .left {
-    margin: 20px;
-  }
-  .right{
+  .linksPart{
     display: flex;
     flex-direction: column;
     align-items: center;
     width: 31.45vw;
     max-width: 452px;
     h3{
-      text-align: center;
       height: 100%;
       display: flex;
-      align-items: flex-end;
+    }
+
+    a { 
+      text-decoration: none;
+      color:#263034;
+      font-size: 2vw;
+      transition: font-size 0.2s;
+      font-weight: bold;
+      @media (min-width:1440px) {
+        font-size: 29px;
+      }
     }
   }
 
-  a { 
-    text-decoration: none;
-    color:#263034;
-    font-size: 2vw;
-    transition: font-size 0.2s;
-    font-weight: bold;
-    :active{
-      color:red;
-    }
-    @media (min-width:1440px) {
-      font-size: 29px;
-    }
-  }
   .wrapper {
     background-color: #FFF312;
-    color: #263034;
     margin: 0px;
     width: 20vw;
     min-height: 275px;
@@ -97,6 +109,56 @@ import { RouterLink } from 'vue-router'
       border-left-width: 144px;
       border-right-width: 144px;
       border-top-width: 72.4px;
+    }
+  }
+
+  @media (max-width:622px){
+    .main{
+      justify-content: flex-end;
+      flex-direction:column-reverse;
+    }
+
+    .TextPart{
+      h2 {
+        width: 200px;
+      }
+      h1 {
+        margin-top: 40px;
+      }
+    }
+
+    .linksPart{
+      width: 100vw;
+      max-width: 100vw;
+      a { 
+        @media (max-width:425px) {
+          font-size: 5vw;
+        }
+        font-size: 4vw;
+      }
+    }
+
+    .wrapper{
+      width: 90vw;
+      min-height: 145px;
+      justify-content: flex-end;
+      max-width: 90vw;
+      nav {
+        z-index: 2;
+        top: 10px;
+        position: relative;
+        :hover{
+          font-size: 4.4vw;
+          @media (max-width:425px) {
+            font-size: 5.8vw;
+          }
+        }
+      }
+    }
+    .triangle{
+      border-left: 45vw solid transparent;
+      border-right: 45vw solid transparent;
+      border-top: 10vw solid #FFF312;
     }
   }
 </style>
