5ad28b2c0f6ad13e266d2ba22abb2222684b5591
GuztaJF-DS <noobshippuden13@gmail.com>
2023-05-15 11:01:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+const rightColor = '#0C4173'
+const leftColor = '#0C4173'
+</script>
+
+
+
+<template>
+  <div class="main">
+    <div class="Left"></div>
+    <div class="Right"></div>
+    <div class="RouterClass">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .main{
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+  }
+  .Left{
+    background-color: v-bind('leftColor');
+    width: 50vw;
+  }
+  .Right{
+    background-color:  v-bind('rightColor');
+    width: 50vw;
+  }
+  .RouterClass{
+    position: absolute;
+    width: 100%;
+    max-width: 1440px;
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="main">
+    <div class="left">
+      <h1>Prazer, meu nome é Gustavo</h1>
+    </div>
+    <div class="right">
+      <div class="wrapper">
+        <nav>
+          <RouterLink active-class="active" to="/about">Sobre Mim</RouterLink>
+          <RouterLink to="/project">Projetos</RouterLink>
+          <RouterLink to="/posts">Posts</RouterLink>
+          <RouterLink to="/study">Estudos</RouterLink>
+        </nav>
+      </div>
+      <div class="triangle"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .main {
+    min-height: 100vh;
+    display: flex;
+    justify-content: space-between;
+    background-color: #0C4173;
+  }
+  .left {
+    margin: 20px;
+  }
+  .right{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 31.45vw;
+    max-width: 452px;
+    h3{
+      text-align: center;
+      height: 100%;
+      display: flex;
+      align-items: flex-end;
+    }
+  }
+
+  a { 
+    text-decoration: none;
+    color:#263034;
+    font-size: 2vw;
+    transition: font-size 0.2s;
+    font-weight: bold;
+    :active{
+      color:red;
+    }
+    @media (min-width:1440px) {
+      font-size: 29px;
+    }
+  }
+  .wrapper {
+    background-color: #FFF312;
+    color: #263034;
+    margin: 0px;
+    width: 20vw;
+    min-height: 275px;
+    max-width: 288px;
+    nav {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      :hover{
+        font-size: 2.4vw;
+      }
+    }
+  }
+  .triangle{
+    border-left: 10vw solid transparent;
+    border-right: 10vw solid transparent;
+    border-top: 5vw solid #FFF312;
+
+    @media (min-width:1440px) {
+      border-left-width: 144px;
+      border-right-width: 144px;
+      border-top-width: 72.4px;
+    }
+  }
+</style>
